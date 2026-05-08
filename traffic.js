25e30602accea179432d8770e3cad9efb18d6977
Damilare <ferzherzee1020@gmail.com>
2026-05-08 10:26:24 +0100

    updatedtrafficlight

--- traffic.js
+++ traffic.js
@@ -1,29 +1,25 @@
- function checkLight() {
-      let color = document.getElementById("colorInput").value.toLowerCase();
-      let light = document.getElementById("trafficLight");
-      let result = "";
+function checkLight() {
+  let color = document.getElementById("colorInput").value.toLowerCase();
+  let light = document.getElementById("trafficLight");
+  let result = "";
 
+  if (color === "red" || color === "yellow" || color === "green") {
+    light.classList.remove("hidden");
 
-      if (color === "red" || color === "yellow" || color === "green") {
-        light.classList.remove("hidden");
-      }
-
-      if (color === "red") {
-        light.style.backgroundColor = "red";
-        result = "Stop! Wait until the light changes.";
-      }
-      if (color === "yellow") {
-        light.style.backgroundColor = "yellow";
-        result = "Get ready to move.";
-      }
-      if (color === "green") {
-        light.style.backgroundColor = "green";
-        result = "Go! You can move now.";
-      }
-      if (color !== "red" && color !== "yellow" && color !== "green") {
-        light.style.backgroundColor = "white";
-        result = " Please enter red, yellow, or green.";
-      }
-
-      document.getElementById("result").innerText = result;
+    if (color === "red") {
+      light.style.backgroundColor = "red";
+      result = "Stop! Wait until the light changes.";
+    } else if (color === "yellow") {
+      light.style.backgroundColor = "yellow";
+      result = "Get ready to move.";
+    } else if (color === "green") {
+      light.style.backgroundColor = "green";
+      result = "Go! You can move now.";
     }
+  } else {
+    light.style.backgroundColor = "white";
+    result = "Please enter red, yellow, or green.";
+  }
+
+  document.getElementById("result").innerText = result;
+}
